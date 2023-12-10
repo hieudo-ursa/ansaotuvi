b8965a8dd37dfda8d375e1fd8fb79fedba3f5586
do.trung.hieu <hieu.do@ursa.vn>
2023-12-10 17:13:23 +0700

    Add new position of sao Thien Phu

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ansaotuvi',
-      version='0.1.2',
+      version='0.1.3',
       description='Chương trình an sao tử vi mã nguồn mở',
       url='https://github.com/hieudo-ursa/ansaotuvi',
       author='hieu.do',
